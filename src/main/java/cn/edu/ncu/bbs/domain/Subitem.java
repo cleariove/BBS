14a396b2a6fab6c1f34febb773b2342cea177566
StephenHe <852630479@qq.com>
2019-12-06 16:27:50 +0800

    实体类生成

---
+++ src/main/java/cn/edu/ncu/bbs/domain/Subitem.java
@@ -0,0 +1,57 @@
+package cn.edu.ncu.bbs.domain;
+
+
+public class Subitem {
+
+  private long subItemId;
+  private String subItemName;
+  private long itemId;
+  private String itemDescription;
+  private long manager;
+
+
+  public long getSubItemId() {
+    return subItemId;
+  }
+
+  public void setSubItemId(long subItemId) {
+    this.subItemId = subItemId;
+  }
+
+
+  public String getSubItemName() {
+    return subItemName;
+  }
+
+  public void setSubItemName(String subItemName) {
+    this.subItemName = subItemName;
+  }
+
+
+  public long getItemId() {
+    return itemId;
+  }
+
+  public void setItemId(long itemId) {
+    this.itemId = itemId;
+  }
+
+
+  public String getItemDescription() {
+    return itemDescription;
+  }
+
+  public void setItemDescription(String itemDescription) {
+    this.itemDescription = itemDescription;
+  }
+
+
+  public long getManager() {
+    return manager;
+  }
+
+  public void setManager(long manager) {
+    this.manager = manager;
+  }
+
+}
