9ce29c1deb41d22be0ab5dc4e51bb26066529e18
刘鑫恺 <31811138+cleariove@users.noreply.github.com>
2019-12-10 19:40:56 +0800

    配置了静态文件的访问

---
+++ src/main/java/cn/edu/ncu/bbs/config/WebConfig.java
@@ -0,0 +1,22 @@
+package cn.edu.ncu.bbs.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+
+public class WebConfig implements WebMvcConfigurer
+{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
+    }
+}
